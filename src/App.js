07ea015fd5f034400dae1292d0994841fb6ddd75
Salil Jagtap <[saliljagtap12@gmail.com]>
2019-08-25 19:27:04 -0700

    my app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Article from './components/Article';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => (
+  <Router>
+        <div>
+          <h1> React Article Application</h1>
+          <ul>
+            <li><Link to = "/">Home</Link></li>
+            <li><Link to = "/article" target="_blank">Article</Link></li>
+          </ul>
+          <Route exact path = "/" component={Home}></Route>
+          <Route exact path = "/article" component={Article}></Route>
+        </div>
+      </Router>
+);
 export default App;
---
+++ src/components/Article.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Article = () => (
+  <div>
+  <div id="links">
+  <ul>
+  <li><a href="#art1">Article 1</a></li>
+  <li><a href="#art2">Article 2</a></li>
+  </ul>
+  </div>
+  <article id="art1">
+  <h1>Breaking News: Tesla and Walmart address solar panel fire issues</h1>
+  <p class="article-meta"><strong>Source:</strong> BBC News, <strong>Published:</strong> 23 August 2019</p>
+  <p>"This is a Random Dummy Data from Internet" US supermarket chain Walmart and Tesla are working to solve the issues which allegedly caused solar panel fires in seven of its stores, Reuters reports.
+        It follows a lawsuit launched by the retailer, alleging "widespread negligence" when the panels were installed on roofs. Clean energy firm SolarCity had installed panels on 240 Walmart stores, and was
+        bought by Tesla in 2016. In a joint statement, the firms said they were addressing the concerns.Every system needed to operate safely, they added.The firms said they were looking to "re-energise Tesla
+        solar installations at Walmart stores, once all parties are certain that all concerns have been addressed". "Above all else, both companies want each and every system to operate reliably, efficiently,
+        and safely," they added. Erlier this week, Walmart launched a lawsuit, asking for the removal of solar panels from more than 240 of its US stores, and claiming the installations had not been completed to
+        proper standards. Court documents described a string of fires that occurred between 2012 and 2018 at Walmart stores in Ohio, Maryland and California. The firms have teamed up on a number of clean energy initiatives,
+        including a pre-order from Walmart of at least 45 Tesla electric lorries.Walmart was seeking compensation for seven fires in its stores which it alleged were caused by faulty solar panels.</p>
+  </article>
+  <article id="art2">
+        <h1>Breaking News: Apple Card can be damaged by wallets and jeans</h1>
+        <p class="article-meta"><strong>Source:</strong> BBC News, <strong>Published:</strong> 22 August 2019</p>
+          <p>"This is a Random Dummy Data from Internet" Apple has advised owners of its new credit card to keep it away from leather and denim. Keeping the card in a leather wallet or in the pocket of a pair of jeans could cause
+          "permanent discolouration". The Apple Card is a relatively plain matt white credit card made of titanium, which was designed to stand out against other credit cards. But people have poked fun at the company after reading
+          that the card could be so easily damaged. Apple has published a guide advising customers how to "safely store and carry" their Apple Card. The guide warns that storing the card with other credit cards can scratch and damage it.
+          "I can say from two months of having the card in my leather wallet, it no longer looks pretty," said one Apple employee on Twitter. Apple staff were able to sign up for the service before the general public. It is currently available
+          to Apple customers in the US. A launch date for UK customers has not been announced. Apple has previously been criticised for making products that are aesthetically pleasing but easily damaged in everyday use. "When not in use, Apple Card
+          should be returned to a uniformly lit white cube," joked Alex Stamos, Facebook's former chief security officer. The Apple Card was created with the help of Goldman Sachs and MasterCard. It was announced in March as part of Apple's push into
+          services. The company also announced a new television streaming platform, gaming portal and enhanced news app.</p>
+  </article>
+</div>
+);
+export default Article;
